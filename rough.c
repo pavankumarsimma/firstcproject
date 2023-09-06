10730de4616c1dd9b8d6e432be08111644ac9c0a
Pavan <pavankumarsimma08@gmail.com>
2023-09-07 00:11:14 +0530

    fsnk

--- rough.c
+++ rough.c
@@ -2,11 +2,7 @@
 
 int main(){
 	printf("Hello World!!");
-<<<<<<< HEAD
 	printf("firsgithubproject");
 	printf("on branch pavan");
-=======
-	printf("dskvnkndvko");
->>>>>>> f59d66e39dcef4e46ef7f405df9d8f49a67f8a5b
 	return 0;
 }
